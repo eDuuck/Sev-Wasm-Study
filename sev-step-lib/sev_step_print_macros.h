1fca402ff9329d902bac6a1bb741fbcd1c31ef31
Luca Wilke <l.wilke@uni-luebeck.de>
2023-03-29 16:40:19 +0200

    added timestamp to flf_printf

--- sev-step-lib/sev_step_print_macros.h
+++ sev-step-lib/sev_step_print_macros.h
@@ -2,6 +2,7 @@
 #define SEV_STEP_PRINT_MACROS
 
 #include <stdio.h>
+#include <time.h>
 
-#define flf_printf(fmt, ...) printf("%s:%d: %s: " fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);
+#define flf_printf(fmt, ...) printf("%ju - %s:%d: %s: " fmt, time(NULL), __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);
 #endif 
